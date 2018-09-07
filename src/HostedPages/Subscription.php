567e2852b11fa6b735d299b25a2b8af7de4af649
Andrew Willis <andrew@willishq.co.uk>
2018-09-07 12:21:18 +0100

    adding a subscription object

--- src/HostedPages/Subscription.php
+++ src/HostedPages/Subscription.php
@@ -4,8 +4,24 @@ namespace ZohoSubscription\HostedPages;
 
 class Subscription
 {
+    private $attributes = [];
+    public function __construct($customerId, $planCode)
+    {
+        $this->attributes = [
+            'customer_id' => $customerId,
+            'plan' => [
+                'plan_code' => $planCode,
+            ]
+        ];
+    }
+
+    public function addRedirectUrl(string $redirectUrl)
+    {
+        $this->attributes['redirect_url'] = $redirectUrl;
+    }
+
     public function toArray(): array
     {
-        return [];
+        return $this->attributes;
     }
 }
--- tests/HostedPages/SubscriptionTest.php
+++ tests/HostedPages/SubscriptionTest.php
@@ -10,8 +10,32 @@ class SubscriptionTest extends TestCase
     /** @test */
     public function itCanCreateASubscriptionObject()
     {
-        $subscription = new Subscription();
+        $customerId = 'c-1234';
+        $planId = 'my-plan';
+        $subscription = new Subscription($customerId, $planId);
 
-        $this->assertEquals([], $subscription->toArray());
+        $this->assertEquals([
+            'customer_id' => $customerId,
+            'plan' => [
+                'plan_code' => $planId,
+            ],
+        ], $subscription->toArray());
+    }
+
+    /** @test */
+    public function itCanAddARedirectUrl()
+    {
+        $customerId = 'c-1234';
+        $planId = 'my-plan';
+        $redirectUrl = 'https://mysite.com/abc/123';
+        $subscription = new Subscription($customerId, $planId);
+        $subscription->addRedirectUrl($redirectUrl);
+        $this->assertEquals([
+            'customer_id' => $customerId,
+            'plan' => [
+                'plan_code' => $planId,
+            ],
+            'redirect_url' => $redirectUrl,
+        ], $subscription->toArray());
     }
 }
